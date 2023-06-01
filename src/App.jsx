37b56e6596a37e5e8d4b71be8d9a761bb2fb460b
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2023-06-01 17:58:25 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useReducer } from 'react'
+import './App.css'
+
+const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete-digit'
+}
+
+function reducer(state, action) {}
+
+export default function App() {
+    const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+    return (
+        <div className='App'>
+            <div className='calculator-grid'>
+                <div className='output'>
+                    <div className='previous-operand'>
+                        {previousOperand} {operation}
+                    </div>
+                    <div className='current-operand'>{currentOperand}</div>
+                </div>
+                <button className='span-two'>AC</button>
+                <button>DEL</button>
+                <button>÷</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>*</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>+</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>-</button>
+                <button>.</button>
+                <button>0</button>
+                <button className='span-two'>=</button>
+            </div>
+        </div>
+    )
+}
